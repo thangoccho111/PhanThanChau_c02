60864474ec20027ce1a861bfa878a3b879fd7c89
thangoccho111 <62010994+thangoccho111@users.noreply.github.com>
2022-05-18 18:08:29 +0700

    fix

--- index.js
+++ index.js
@@ -3,5 +3,15 @@ var taikhoan = {
   tendangnhap: "Chau",
   matkhau: "123456",
   email: "chau00@email.com",
-  phone: "+846738267889",
+  phone: "846738267889",
 };
+console.log(taikhoan);
+
+function NhapThongTin(taikhoan) {
+  taikhoan.tendangnhap = prompt("Nhập tên đăng nhập của bạn:");
+  taikhoan.matkhau = prompt("Nhập mật khẩu của bạn:");
+  taikhoan.email = prompt("Nhập thông tin email của bạn:");
+  taikhoan.phone = prompt("Nhập số điện thoại của bạn:");
+}
+
+// let Tong =
